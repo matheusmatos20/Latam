ef982797585ba729225e6b0d2e3311ed5bdb9c28
matheusmatos20 <matheus.matos@fatec.sp.gov.br>
2024-11-15 01:13:38 -0300

    Creando la funcion contador de mencao

--- src/q3_time.py
+++ src/q3_time.py
@@ -12,3 +12,20 @@ def retorna_jsons(filepath):
                 Array_jsons.append(json_data) #salvando json en array
     return Array_jsons
 
+
+
+def contar_mencoes(jsons):
+    mention_counter = Counter()
+
+    for tweet in jsons:
+        content = tweet.get("content", "")
+
+        # Expressão regular para capturar menções de usuários (@username)
+        pattern = r'@([A-Za-z0-9_]+)'
+
+        # Encontrar todas as menções no conteúdo
+        mentions = re.findall(pattern, content)
+        # Conta a ocorrência de cada emoji
+        mention_counter.update(mentions)
+         
+    return mention_counter #list(emoji_counter.items())

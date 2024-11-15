8cf50ef79e363ef4d31d44b6c74f7fcd46372e03
matheusmatos20 <matheus.matos@fatec.sp.gov.br>
2024-11-15 01:14:26 -0300

    Creacion de def q3_time oara buscar top 10 emojis

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,9 +1,25 @@
 from typing import List, Tuple
+import json
+from collections import Counter
+import re
+from typing import List, Tuple
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
-    
+    resultado =[]
+    mention_count = contar_mencoes(retorna_jsons(file_path))
 
+    # Top 10 emojis mais frequentes
+    top_10_users = mention_count.most_common(10)
+
+    # Exibir os resultados
+    # print("Top 10 emojis mais frequentes:")
+    for mention, count in top_10_users:
+        resultado.append((mention, count))
+        # print(f"{emoji_char}: {count}")
+    return resultado
+        # print(f"{emoji_char}: {count}")
+#Los top 10 emojis más usados con su respectivo conteo. Debe incluir las siguientes funciones:
+    
 def retorna_jsons(filepath):
     Array_jsons=[]    
     with open(filepath, 'r', encoding='utf-8') as file:
@@ -11,7 +27,6 @@ def retorna_jsons(filepath):
                 json_data = json.loads(line.strip())  # Usando .strip() para remover las quebras de lineas extras
                 Array_jsons.append(json_data) #salvando json en array
     return Array_jsons
-
 
 
 def contar_mencoes(jsons):
@@ -29,3 +44,9 @@ def contar_mencoes(jsons):
         mention_counter.update(mentions)
          
     return mention_counter #list(emoji_counter.items())
+
+# file_path = "C:\\Users\\mathe\\Documents\\Desafio Latam\\farmers-protest-tweets-2021-2-4.json"
+# # Contar os emojis nos tweets
+
+# data = q3_time(file_path)
+# print(data)

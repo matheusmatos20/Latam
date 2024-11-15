bf4bb736e0b9ed8e6b9a35594b53d249f050886d
matheusmatos20 <matheus.matos@fatec.sp.gov.br>
2024-11-15 01:07:54 -0300

    Creando la funcion retorna_jsons

--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,19 @@
 from typing import List, Tuple
+from datetime import datetime
+import json
+from collections import Counter
+import emoji
+from typing import List, Tuple
+
+
+def retorna_jsons(filepath):
+    Array_jsons=[]    
+    with open(filepath, 'r', encoding='utf-8') as file:
+            for line in file:
+                json_data = json.loads(line.strip())  # Usando .strip() para remover las quebras de lineas extras
+                Array_jsons.append(json_data) #salvando json en array
+    return Array_jsons
+
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
     pass

4061cd60f18b45efb0c17b11df99fc744092b453
matheusmatos20 <matheus.matos@fatec.sp.gov.br>
2024-11-15 01:03:15 -0300

    Creacion de llamada de teste de observacion sql

--- src/q1_time.py
+++ src/q1_time.py
@@ -63,3 +63,35 @@ def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
     except ValueError as e:
             print(f"Erro ao converter data: {e}")
     
+
+##-------------------------------------TESTE ----------------------------------##
+# file_path = "C:\\Users\\mathe\\Documents\\Desafio Latam\\farmers-protest-tweets-2021-2-4.json"
+# data = q1_time(file_path)
+# print(data)
+
+
+
+# --------------------------------------------------------OBSERVAÇÃO ---------------------------------------------------------#
+# Se fosse um banco de dados relacional e tivessemos as tabelas Tweet, TweetUser, User, para resolver esse exercicio seria somente necessário o comando abaixo
+# Las top 10 fechas donde hay más tweets. Mencionar el usuario (username) que más publicaciones tiene por cada uno de esos días.
+
+# with Max_tweets as (
+#         select top 10 count(*) as qtd ,t.date 
+#         from tweet t
+#         inner join user u on u.user_id = t.user_id
+#         group by t.date
+#         order by 1 desc)
+# , count_users as (
+#      select t.date, u.username,Count(*) as qtd
+#         from tweet t
+#         inner join user u on u.user_id = t.user_id
+#         inner join cte  c on c.date = t.date
+#         group by t.date,u.username
+# )
+# , top_users as (
+#      select date,username, qtd, row_number() over(partition by date, order by qtd desc) as rank
+#      from count_users
+# )
+# select date,username
+# from top_users
+# where rank=1
